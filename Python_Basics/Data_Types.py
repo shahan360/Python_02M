f28ef30602d4eadc3adeb5a8cbfc53e29b717da4
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2024-03-06 00:40:42 -0600

    commited on 6th March 2024

---
+++ Python_Basics/Data_Types.py
@@ -0,0 +1,15 @@
+#Fundamental Data Types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+#Classes -> custom types #We can create our own data types
+
+#Specialized Data Types #Specialized packages and modules
+
+None #absence of value or null value
