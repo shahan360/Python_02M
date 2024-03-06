bcec89133d72ee253f5183b729ad361d174e5ede
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2024-03-06 01:03:26 -0600

    Merge Commit on 6th March 2024

---
+++ Python_Basics/Int_Float.py
@@ -0,0 +1,12 @@
+#Fundamental Data Types in Python
+# int and float are the two most common data types in Python.
+print(type(2 + 4))
+print(type(2 - 4))
+print(type(2 * 4))
+print(type(2 / 4))
+
+print(type(9.1+1.1))
+
+print(2 ** 3) #2 to the power of 3
+print(5 // 4) #floor division
+print(5 % 4) #modulus
